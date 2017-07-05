43083d99b82a8dcbb047cd79476ebf6578096781
Jim <Jim@JIM-PC>
2017-07-06 05:12:20 +0800

    Add Json Model

--- 地址轉經緯度/Program.cs
+++ 地址轉經緯度/Program.cs
@@ -12,80 +12,6 @@ using System.Windows.Forms;
 
 namespace 地址轉經緯度
 {
-    public class Rootobject
-    {
-        public Result[] results { get; set; }
-        public string status { get; set; }
-    }
-
-    public class Result
-    {
-        public Address_Components[] address_components { get; set; }
-        public string formatted_address { get; set; }
-        public Geometry geometry { get; set; }
-        public bool partial_match { get; set; }
-        public string place_id { get; set; }
-        public string[] types { get; set; }
-    }
-
-    public class Geometry
-    {
-        public Bounds bounds { get; set; }
-        public Location location { get; set; }
-        public string location_type { get; set; }
-        public Viewport viewport { get; set; }
-    }
-
-    public class Bounds
-    {
-        public Northeast northeast { get; set; }
-        public Southwest southwest { get; set; }
-    }
-
-    public class Northeast
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
-    }
-
-    public class Southwest
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
-    }
-
-    public class Location
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
-    }
-
-    public class Viewport
-    {
-        public Northeast1 northeast { get; set; }
-        public Southwest1 southwest { get; set; }
-    }
-
-    public class Northeast1
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
-    }
-
-    public class Southwest1
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
-    }
-
-    public class Address_Components
-    {
-        public string long_name { get; set; }
-        public string short_name { get; set; }
-        public string[] types { get; set; }
-    }
-
-
     class Program
     {
         static void Main(string[] args)
@@ -125,7 +51,7 @@ namespace 地址轉經緯度
 
                     }
                     if (Id % 100 == 0)
-                        Thread.Sleep(10);
+                        Thread.Sleep(1);
                     Id++;
                 }
                 str.Close();
@@ -142,7 +68,6 @@ namespace 地址轉經緯度
 
                 //    goto fuck;
                 //}
-
 
             }
             catch
